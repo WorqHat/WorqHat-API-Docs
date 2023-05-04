32311bb880bb4d2cdacf49dc9a8f2190a6dbdd80
Sagnik Ghosh <sagnik@worqhat.com>
2023-05-04 23:38:11 +0530

    fix Configure the Initial Page
    Took 42 minutes

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,17 +1,22 @@
 import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
+import Image from 'next/image'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: (
+      <>
+        <img src={'https://www.worqhat.com/resources/WorqHat%20TM%20Logo.png'} width={130} height={130} alt={'WorqHat Logo'}></img>
+      </>
+  ),
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://github.com/WorqHat/WorqHat-API-Docs',
   },
   chat: {
-    link: 'https://discord.com',
+    link: 'https://discord.com/invite/KHh9mguKBx',
   },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase: 'https://github.com/WorqHat/WorqHat-API-Docs',
   footer: {
-    text: 'Nextra Docs Template',
+    text: '© 2023. Winlysis Pvt. Ltd., India All rights reserved.',
   },
 }
 
